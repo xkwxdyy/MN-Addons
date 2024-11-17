1282f4feeecaaf1556f89c3055a6ad1084b432c4
xkwxdyy <kangweixia_xdyy@163.com>
2024-11-17 10:55:58 +0800

    更新脚本

--- update.py
+++ update.py
@@ -124,3 +124,67 @@ for file in extra_files:
 if not files_found:
     print("没有新文件")
     shutil.rmtree(newVersion_path)
+    
+    
+
+# 再更新一遍 official 版本
+# 旧版本地址
+oldVersion_path = os.path.join(os.getcwd(), 'mntoolbar_official')
+
+# 调试信息
+print(f"正在解压文件：{target_addon}")
+
+# 检查是否需要解压，存在时是否强制覆盖（这里没有覆盖检查）
+if os.path.exists(newVersion_path) and os.path.isdir(newVersion_path):
+    shutil.rmtree(newVersion_path)
+
+# 执行命令行命令：mnaddon unpack target_addon
+try:
+    command = f"mnaddon unpack {target_addon}"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(result.stdout)
+    if result.returncode != 0:
+        print(f"解压失败：{result.stderr}")
+        exit()
+    print("解压成功")
+except subprocess.CalledProcessError as e:
+    print(f"解压失败：{e}")
+    exit()
+
+# 检查新解压目录是否存在
+if not os.path.isdir(newVersion_path):
+    print(f"解压目录 {newVersion_path} 不存在")
+    exit()
+
+# 将 newVersion_path 中的 .js 文件和 .html 文件复制到 oldVersion_path 中 
+for file in os.listdir(newVersion_path):
+    if file.endswith('.js') or file.endswith('.html') or file.endswith('.json'):
+        new_file = os.path.join(newVersion_path, file)
+        old_file = os.path.join(oldVersion_path, file)
+        if os.path.exists(old_file):
+            if filecmp.cmp(new_file, old_file):
+                print(f"{file} 未发生变化，跳过")
+                continue
+        print(f"复制文件：{new_file} -> {old_file}")
+        shutil.copy(new_file, old_file)
+
+
+# 输出 newVersion_path 中 oldVersion_path 没有的文件
+old_files = set(os.listdir(oldVersion_path))
+new_files = set(os.listdir(newVersion_path))
+
+extra_files = new_files - old_files
+
+# 定义用于匹配 "customx.png" 文件的正则表达式
+pattern = re.compile(r'custom\d+\.png')
+
+files_found = False
+
+for file in extra_files:
+    if not pattern.match(file):
+        print(f"文件 {file} 在 newVersion_path 中存在，但在 oldVersion_path 中不存在")
+        files_found = True
+
+if not files_found:
+    print("没有新文件")
+    shutil.rmtree(newVersion_path)

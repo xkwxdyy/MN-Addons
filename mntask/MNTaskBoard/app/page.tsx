7f60ee376bbf875d9d26fe7dbb76fbec5fef1af5
xkwxdyy <kangweixia_xdyy@163.com>
2025-08-07 16:33:43 +0800

    去掉下载 MNtask 应用提示

--- mntask/MNTaskBoard/app/page.tsx
+++ mntask/MNTaskBoard/app/page.tsx
@@ -1,11 +1,11 @@
 import MNTaskBoard from "@/components/mntask-board"
-import { PWARegister } from "@/components/pwa-register"
+// import { PWARegister } from "@/components/pwa-register"
 
 export default function Home() {
   return (
     <>
       <MNTaskBoard />
-      <PWARegister />
+      {/* <PWARegister /> */}
     </>
   )
 }
